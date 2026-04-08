8dfad58e86e80bd7ec5b3272518a2df0996222f4
ariannecope <arianncope@gmail.com>
2026-04-08 10:52:44 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/About';
+import About from './pages/About';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
